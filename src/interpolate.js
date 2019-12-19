54bcdc4559ba010018ce70ce50375a4db2da2f9f
Nicolás Fantone <nicolasfantone@flybondi.com>
2019-12-19 18:59:15 -0300

    feat(ramda): add interpolate function (#57)

---
+++ src/interpolate.js
@@ -0,0 +1,28 @@
+'use strict';
+const curry = require('./curry');
+const { rejectNil } = require('./reject-nil');
+const { compose, values, mapObjIndexed, reduce, replace, flip, call } = require('ramda');
+
+const replacer = compose(
+  values,
+  mapObjIndexed((value, key) => replace(`{${key}}`, value)),
+  rejectNil
+);
+
+/**
+ * Replaces variables in a template enclosed by `{}`.
+ *
+ * @examples
+ *  interpolate('I am {name}', { name: 'Error' });
+ *  // 'I am Error'
+ *
+ * @param {String} template The template to interpolate.
+ * @param {Object} context The values to replace in `template.
+ * @returns {String} The result of resolving template interpolation
+ *  with the given `context`.
+ */
+function interpolate(template, context) {
+  return reduce(flip(call), template, replacer(context));
+}
+
+module.exports = curry(interpolate);
