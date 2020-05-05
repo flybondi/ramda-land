9602d60b5ffdc9e40d6514f65a1dded4df48d3e2
Luciano Fantone <lucianoafantone@gmail.com>
2020-05-05 17:54:56 +0200

    test(ramda): change return value of booleanProp

--- src/boolean-prop.test.js
+++ src/boolean-prop.test.js
@@ -11,7 +11,7 @@ describe('the `booleanProp` function', () => {
   });
 
   test('should return null', () => {
-    expect(booleanProp('pokedex', {})).toEqual(null);
+    expect(booleanProp('pokedex', {})).toEqual(undefined);
   });
 });
 
