6e51f631516ceecc9cfc72e810facd301f6cba50
ivelazco <ignaciovelazco12@gmail.com>
2020-06-12 17:40:50 -0300

    chore: change test descriptions

--- src/round-number.test.js
+++ src/round-number.test.js
@@ -1,47 +1,47 @@
 'use strict';
 const roundNumber = require('./round-number');
 
-test('should return the number rounder to 0 decimals, rounding up when has to', () => {
+test('should return the number rounded to 0 decimals, rounding up when has to', () => {
   expect(roundNumber(0, 2.8)).toBe(3);
   expect(roundNumber(0, -2.8)).toBe(-3);
 });
 
-test('should return the number rounder to 0 decimals, rounding down when has to', () => {
+test('should return the number rounded to 0 decimals, rounding down when has to', () => {
   expect(roundNumber(0, 2.4)).toBe(2);
   expect(roundNumber(0, -2.4)).toBe(-2);
 });
 
-test('should return the number rounder to 1 decimals, rounding up when has to', () => {
+test('should return the number rounded to 1 decimals, rounding up when has to', () => {
   expect(roundNumber(1, 2.88)).toBe(2.9);
   expect(roundNumber(1, -2.88)).toBe(-2.9);
 });
 
-test('should return the number rounder to 1 decimals, rounding down when has to', () => {
+test('should return the number rounded to 1 decimals, rounding down when has to', () => {
   expect(roundNumber(1, 2.83)).toBe(2.8);
   expect(roundNumber(1, -2.83)).toBe(-2.8);
 });
 
-test('should return the number rounder to 2 decimals, rounding up when has to', () => {
+test('should return the number rounded to 2 decimals, rounding up when has to', () => {
   expect(roundNumber(2, 2.836)).toBe(2.84);
   expect(roundNumber(2, -2.836)).toBe(-2.84);
 });
 
-test('should return the number rounder to 2 decimals, rounding down when has to', () => {
+test('should return the number rounded to 2 decimals, rounding down when has to', () => {
   expect(roundNumber(2, 2.832)).toBe(2.83);
   expect(roundNumber(2, -2.832)).toBe(-2.83);
 });
 
-test('should return the number rounder to 3 decimals, rounding up when has to', () => {
+test('should return the number rounded to 3 decimals, rounding up when has to', () => {
   expect(roundNumber(3, 2.8236)).toBe(2.824);
   expect(roundNumber(3, -2.8236)).toBe(-2.824);
 });
 
-test('should return the number rounder to 3 decimals, rounding down when has to', () => {
+test('should return the number rounded to 3 decimals, rounding down when has to', () => {
   expect(roundNumber(3, 2.8234)).toBe(2.823);
   expect(roundNumber(3, -2.8234)).toBe(-2.823);
 });
 
-test('should return the number rounder to 3 decimals, rounding up when is in the middle', () => {
+test('should return the number rounded to 3 decimals, rounding up when is in the middle', () => {
   expect(roundNumber(3, 2.8235)).toBe(2.824);
   expect(roundNumber(3, -2.8235)).toBe(-2.823);
 });
